dd4375cde1b5c3d1e07d063afd6eaa8cc9fa13cb
IsaacGHoward <howardisaac100@gmail.com>
2020-10-28 16:19:39 -0700

    Home button works, got rid of some unnecessary imports

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,11 +1,5 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useParams
-} from "react-router-dom";
+import { Link } from "react-router-dom";
 export default function MovieList(props) {
   return (
     <div className="movie-list">
