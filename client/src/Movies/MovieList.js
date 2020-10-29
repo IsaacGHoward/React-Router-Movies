30402a77530ec100faa4e8f179a12ebe5f03b227
IsaacGHoward <howardisaac100@gmail.com>
2020-10-28 17:01:40 -0700

    Got rid of comments that were no longer needed

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -14,20 +14,3 @@ export default function MovieList(props) {
   );
 }
 
-/*
-function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
-
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-    </div>
-  );
-}
-*/
